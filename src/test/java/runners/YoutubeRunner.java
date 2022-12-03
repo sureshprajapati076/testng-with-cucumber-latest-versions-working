9382e3b806dde48966baba42cfc536104c27881f
sureshprajapati076 <sureshprajapati076@gmail.com>
2022-12-02 22:38:27 -0800

    initial commit

---
+++ src/test/java/runners/YoutubeRunner.java
@@ -0,0 +1,68 @@
+package runners;
+
+
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+
+import java.io.*;
+
+@CucumberOptions(
+        glue = "stepdefs",
+        plugin = {"pretty"},
+        monochrome = true,
+        features = "src/test/resources/Youtube",
+        tags = "@youtube")
+public class YoutubeRunner extends AbstractTestNGCucumberTests {
+
+
+
+    @BeforeSuite
+    public void beforeSuite() throws IOException {
+        System.out.println("BEFORE SUITE EXECUTED!");
+        System.out.println("HI");
+/*
+        File featureFile= new File("src/test/resources/Youtube/YouTube.feature");
+        BufferedWriter writer = null;
+        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(featureFile),"UTF-8"));
+
+        writer.write("Feature: YouTube Feature Dec 2022\n" +
+                "\n" +
+                "  @youtube\n" +
+                "  Scenario Outline: Search text Automation in youtube\n" +
+                "    Given User is in youtube \"<homepage>\" homepage\n" +
+                "    When User enters text \"<searchText>\" in search box\n" +
+                "    And User Click search Button\n" +
+                "    Then Verify user sees results\n" +
+                "\n" +
+                "    Examples:\n" +
+                "      |homepage|searchText|\n" +
+                "      |https://youtube.com|Electronics|\n" +
+                "\n" +
+                "\n" +
+                "  @youtube\n" +
+                "  Scenario Outline: Search text Automation in youtube Ditital\n" +
+                "    Given User is in youtube \"<homepage>\" homepage\n" +
+                "    When User enters text \"<searchText>\" in search box\n" +
+                "    And User Click search Button\n" +
+                "    Then Verify user sees results\n" +
+                "\n" +
+                "    Examples:\n" +
+                "      | homepage            | searchText |\n" +
+                "      | https://youtube.com | Digital    |");
+
+        writer.close();
+
+ */
+    }
+
+//    @Override
+//    @DataProvider(parallel = true)
+//    public Object[][] scenarios() {
+//        return super.scenarios();
+//    }
+
+}
---
+++ src/test/java/stepdefs/YoutubeSteps.java
@@ -0,0 +1,33 @@
+package stepdefs;
+
+
+import cucumber.api.java.en.*;
+
+public class YoutubeSteps {
+
+    @Given("User is in youtube {string} homepage")
+    public void user_is_in_youtube_homepage(String string) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("User enters text {string} in search box")
+    public void user_enters_text_in_search_box(String string) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("User Click search Button")
+    public void user_Click_search_Button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("Verify user sees results")
+    public void verify_user_sees_results() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+
+}
