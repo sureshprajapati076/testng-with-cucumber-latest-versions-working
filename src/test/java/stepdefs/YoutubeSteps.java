9e1ee6a92173097005e46061fb44664d6e65f246
sureshprajapati076 <sureshprajapati076@gmail.com>
2022-12-07 17:12:10 -0800

    html parser jsoup

--- src/test/java/stepdefs/YoutubeSteps.java
+++ src/test/java/stepdefs/YoutubeSteps.java
@@ -5,6 +5,9 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.*;
 import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -22,26 +25,31 @@ import java.time.LocalDateTime;
 public class YoutubeSteps {
 
 
-    WebDriver webDriver=null;
+    WebDriver webDriver = null;
 
     SoftAssert softAssert = new SoftAssert();
 
     @Before
-    public void setupDriver(){
+    public void setupDriver() {
 
-      //  System.setProperty("webdriver.chrome.driver","C:/Users/sures/Documents/chromedriver.exe");
+        //  System.setProperty("webdriver.chrome.driver","C:/Users/sures/Documents/chromedriver.exe");
 
-        ChromeOptions options= new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         webDriver = new ChromeDriver(options);
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-        webDriver.get("https://youtube.com");
+        webDriver.get("https://citibank.com");
 
-        File scrFile = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+
+//      Document document = Jsoup.parse(webDriver.getPageSource());
+//      Elements elements = document.selectXpath("//*[@id='signInBtn']");
+
+
+        File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
 
         try {
-            String fileName="output-screenshots/Screenshot_"+LocalDateTime.now().toString().replace(":","-")+".png";
+            String fileName = "output-screenshots/Screenshot_" + LocalDateTime.now().toString().replace(":", "-") + ".png";
             FileUtils.copyFile(scrFile, new File(fileName));
 
             //if we need to attach file to scenario for reporting.
@@ -57,10 +65,9 @@ public class YoutubeSteps {
     public void user_is_in_youtube_homepage(String string) {
         // Write code here that turns the phrase above into concrete actions
 
-      //  Assert.assertEquals(string,"https://youtube.com","URL is not Youtube related");
+        //  Assert.assertEquals(string,"https://youtube.com","URL is not Youtube related");
 
-     //   softAssert.assertEquals(string,"https://youtube.com1","did not match url");
-
+        //   softAssert.assertEquals(string,"https://youtube.com1","did not match url");
 
 
         //softAssert.assertAll();
@@ -80,27 +87,27 @@ public class YoutubeSteps {
     public void user_Click_search_Button() {
         // Write code here that turns the phrase above into concrete actions
 
-        softAssert.assertTrue(false,"forced failing case");
+        softAssert.assertTrue(false, "forced failing case");
 
     }
 
     @Then("Verify user sees results")
     public void verify_user_sees_results() {
         // Write code here that turns the phrase above into concrete actions
-    //    webDriver.quit();
+        //    webDriver.quit();
         try {
             Thread.sleep(10);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-     //   softAssert.assertAll();
+        //   softAssert.assertAll();
 
     }
 
     @AfterStep
-    public void afterStep(Scenario scenario){
+    public void afterStep(Scenario scenario) {
         System.out.println(scenario.getSourceTagNames());
-        System.out.println("CURRENT THREAD: "+Thread.currentThread().getId());
+        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId());
     }
 
 
