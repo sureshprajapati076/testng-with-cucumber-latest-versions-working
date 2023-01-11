69555988cf58e8de1927ba50c18e297bda855583
sureshprajapati <sureshprajapati076@gmail.com>
2023-01-11 09:06:03 -0800

    highlighting selected element with red border.. fun

--- src/test/java/stepdefs/CommonActions.java
+++ src/test/java/stepdefs/CommonActions.java
@@ -4,11 +4,14 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import utils.PropertiesReaderUtils;
 import utils.SeleniumDriver;
+
+import java.time.Duration;
 
 public class CommonActions {
     public static ThreadLocal<Scenario> scenarioThreadLocal = new ThreadLocal<>();
@@ -35,6 +38,8 @@ public class CommonActions {
     }
 
     public static void performClick(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) SeleniumDriver.getDriver();
+        js.executeScript("arguments[0].style.border='3px solid red'", element);
         Actions actions = new Actions(SeleniumDriver.getDriver());
         actions.moveToElement(element).click().build().perform();
     }
